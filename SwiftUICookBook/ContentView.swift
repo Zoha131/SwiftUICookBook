a3e849cd5b1639c35b67a201eb2bb15b4d122d78
zoha131 <abirhasanzoha@gmail.com>
2021-01-21 04:21:04 +0600

    Setting View

--- SwiftUICookBook/ContentView.swift
+++ SwiftUICookBook/ContentView.swift
@@ -8,48 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State var query = ""
-  @State var items = (0...10).map { $0  }
   var body: some View {
     NavigationView {
-      ScrollView {
-        LazyVStack(spacing: 20) {
-
-          TextField("Search", text: $query)
-            .frame(minHeight: 44)
-            .background(Color.gray)
-            .cornerRadius(15)
-            .padding()
-
-          TitleView(title: "Ingreadiets")
-        }
-
-      }
-      .listStyle(PlainListStyle())
-      .navigationTitle("Recipe Shope")
-      .navigationBarTitleDisplayMode(.inline)
-      .navigationBarItems(
-        leading: Button(action: {}) {
-          Image(uiImage: UIImage(named: "ic_menu")!)
-        },
-        trailing: Button(action: {}) {
-          Image(uiImage: UIImage(named: "ic_notification")!)
-        }
-      )
-    }
-  }
-}
-
-struct TitleView: View {
-  let title: String
-
-  var body: some View {
-    HStack {
-      Text(title)
-      Spacer()
-      Button(action: {}) {
-        Image(systemName: "chevron.right.circle")
-      }
+      NavigationLink("Setting", destination: SettingsView())
     }
   }
 }
